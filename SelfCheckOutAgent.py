81bf62f595ec3a460ef56cdba03ac586531c69eb
BeeTeePhoneHome <60676566+BeeTeePhoneHome@users.noreply.github.com>
2020-03-05 07:23:26 -0800

    Update SelfCheckOutAgent.py
    Copy of employee, but different speed per item

--- SelfCheckOutAgent.py
+++ SelfCheckOutAgent.py
@@ -1,3 +1,18 @@
-class SelfCheckOutAgent(object):
+import queue
+
+class SelfCheckOutAgent(EmployeeCheckOutAgent):
     def __init__(self):
-        pass
+        self.secPerItem= 5
+        self.customers = queue.Queue()
+        self.current_customer = None
+        eventClock = 0
+    
+    def process(self):
+        self.current_customer = self.current_customer if self.current_customer != None else self.customers.get()
+        self.current_customer.process_with(self.secPerItem)
+
+    def tick(self):
+        self.eventClock = self.eventClock + 1
+
+    def addToLine(self, customer):
+        self.customers.put(customer)
